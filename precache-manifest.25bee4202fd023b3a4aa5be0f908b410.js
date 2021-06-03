a4a49ac1323dda7d1e59c7610952c5f18b25842f
startsourceW <startnexus@gmail.com>
2021-06-03 21:40:39 +0900

    Updates

---
+++ precache-manifest.25bee4202fd023b3a4aa5be0f908b410.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0fb40d375970ddfc4db",
+    "url": "/beforecast/static/js/app.0c42b90a.chunk.js"
+  },
+  {
+    "revision": "a71b6c98d196548a3a58",
+    "url": "/beforecast/static/js/runtime~app.6b5ed87b.js"
+  },
+  {
+    "revision": "28555699f86203d327d6",
+    "url": "/beforecast/static/js/2.cfc4d3ab.chunk.js"
+  },
+  {
+    "revision": "d6f5cd94a50b00f50e6c23b7d62ecc3c",
+    "url": "/beforecast/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/beforecast/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/beforecast/expo-service-worker.js"
+  },
+  {
+    "revision": "f4c758542a6613cd2dce057d8f50ed2a",
+    "url": "/beforecast/register-service-worker.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/beforecast/static/js/2.cfc4d3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bdc71fef88020f6877fa700ed24c55",
+    "url": "/beforecast/favicon-16.png"
+  },
+  {
+    "revision": "4289b3255403ab44e56bdedc7d3eb627",
+    "url": "/beforecast/favicon-32.png"
+  },
+  {
+    "revision": "b5b3de034882940e25908139010fc847",
+    "url": "/beforecast/favicon.ico"
+  },
+  {
+    "revision": "07cf80d1bdff113a11f9b161bc793c46",
+    "url": "/beforecast/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/beforecast/expo-service-worker.js",{scope:"/beforecast/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
